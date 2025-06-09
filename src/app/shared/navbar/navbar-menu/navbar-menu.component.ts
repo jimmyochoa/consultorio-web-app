02c29c01f65f3338cdd07f91c362c424c86c8f8d
asve06 <asvega06@gmail.com>
2025-06-09 16:13:59 -0500

    Feature: implement navbar component with derived elements and its global CSS styles

--- src/app/shared/navbar/navbar-menu/navbar-menu.component.ts
+++ src/app/shared/navbar/navbar-menu/navbar-menu.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarItemComponent } from '../navbar-item/navbar-item.component';
+import { NavbarToggleComponent } from '../navbar-toggle/navbar-toggle.component';
 
 @Component({
   selector: 'app-navbar-menu',
-  imports: [],
+  imports: [NavbarItemComponent, NavbarToggleComponent],
   templateUrl: './navbar-menu.component.html',
   styleUrl: './navbar-menu.component.css'
 })
