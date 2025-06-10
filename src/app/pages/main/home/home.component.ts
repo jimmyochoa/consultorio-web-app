105d0d5e360c90bd120ed7176f11225ec9e804a6
Leynder <leynder.elian@gmail.com>
2025-06-10 11:18:16 -0500

    feature(home): implement homepage and components design using Bootstrap and custom CSS styles

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CarouselSlideComponent } from '../../../shared/carousel-slide/carousel-slide.component';
+import { FeaturedItemComponent } from '../../../shared/featured-item/featured-item.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CarouselSlideComponent, FeaturedItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
