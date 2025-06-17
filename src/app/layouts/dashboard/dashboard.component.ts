1b9a7755421424324e08f7e2fa85f7701eb8b478
asve06 <asvega06@gmail.com>
2025-06-17 01:47:39 -0500

    Feature(sidebar): add sidebar with structured components and shared CSS styles

--- src/app/layouts/dashboard/dashboard.component.ts
+++ src/app/layouts/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../shared/sidebar/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/shared/sidebar/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SidebarMenuItemComponent } from '../sidebar-menu-item/sidebar-menu-item.component';
 
 @Component({
   selector: 'app-sidebar-menu',
-  imports: [],
+  imports: [SidebarMenuItemComponent],
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.css'
 })
 export class SidebarMenuComponent {
 
 }
+
